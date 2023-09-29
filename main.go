d934d97484f79aad64e1c0450b896cc65790c149
Nicolas <nicolasmartin0404@gmail.com>
2023-09-29 17:38:43 +0200

    Database layer added

--- main.go
+++ main.go
@@ -1,20 +1,46 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"github.com/Volgar04/hotel-reservation/api"
+	"github.com/Volgar04/hotel-reservation/db"
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
 )
+
+const dbURI = "mongodb://localhost:27017"
+const dbName = "hotel-reservation"
+const userColl = "users"
+
+var config = fiber.Config{
+	ErrorHandler: func(c *fiber.Ctx, err error) error {
+		return c.JSON(map[string]string{"error": err.Error()})
+	},
+}
 
 func main() {
 	listenAddr := flag.String("listenAddr", ":5002", "server listen address")
 	flag.Parse()
 
-	app := fiber.New()
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dbURI))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// handlers initializations
+	userHandler := api.NewUserHandler(db.NewMongoUserStore(client))
+
+	app := fiber.New(config)
 	apiV1 := app.Group("/api/v1")
 
-	apiV1.Get("/user", api.HandleGetUsers)
-	apiV1.Get("/user/:id", api.HandleGetUser)
+	apiV1.Get("/user", userHandler.HandleGetUsers)
+	apiV1.Get("/user/:id", userHandler.HandleGetUser)
 
-	app.Listen(*listenAddr)
+	err = app.Listen(*listenAddr)
+	if err != nil {
+		panic(err)
+	}
 }
